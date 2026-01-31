49cfa6df810156f4f4538b5b360aaaccdf4e0d9c
Parvathamabhinav <onlyforfreefire51@gmail.com>
2026-01-31 12:25:59 +0530

    Update delete and working clean

--- app.js
+++ app.js
@@ -26,7 +26,31 @@ app.post('/create', async (req, res) => {
   } catch (error) {
     res.status(500).send('Error creating user: ' + error.message);
   }
-});  
+});
+
+app.get('/delete/:id', async (req, res) => {
+  let userId = req.params.id;
+  try {
+    await userModel.findByIdAndDelete(userId);
+    res.redirect('/read');
+  }
+  catch (error) {
+    res.status(500).send('Error deleting user: ' + error.message);
+  } 
+});
+
+app.get('/edit/:id', async (req, res) => {
+  res.render('edit', { userId: req.params.id });
+});
+
+app.post('/edit', async (req, res) => {
+  let { id, name, email, image } = req.body;  
+  try {
+    await userModel.findByIdAndUpdate(id, { name, email, image });
+    res.redirect('/read');
+  } catch (error) {
+    res.status(500).send('Error updating user: ' + error.message);
+  }});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
