7f13b7e6c83db54db8b8c97b9614fce34ce92125
Parvathamabhinav <onlyforfreefire51@gmail.com>
2026-02-01 19:30:02 +0530

    added go back

--- models/user.js
+++ models/user.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/mydatabase');
 const userSchema = new mongoose.Schema({
     name: String,       
-    email: String,
-    image: String
+    email: { type: String, unique: true },
+    image: String,
+    password: String
 });
 const user = mongoose.model('user', userSchema);
 module.exports = user;    
